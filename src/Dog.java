04c2274c0a143471b008e19bad902e8ba8b82da5
helendai <helend2027@student.cis.edu.hk>
2025-01-25 00:03:20 +0800

    yay

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,63 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String color;
+    private Owner owner;
+
+    public Dog(int age, String name, String color, Owner owner){
+        this.age = age;
+        this.name = name;
+        this.color = color;
+        this.owner = owner;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public Owner getOwner(){
+        return owner;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setOwner(Owner owner){
+        this.owner = owner;
+    }
+
+    private void addOneAge(){
+        age++;
+    }
+
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+
+    public String toString(){
+        return "This dog is named " + name + " and is " + age + " years old. This dog is " + color + ".";
+    }
+
+//    public static void main(String[] args){
+//        Dog dog = new Dog(12, "Billy", "red", "Helen");
+//        System.out.println(dog);
+//    }
 }
--- src/Owner.java
+++ src/Owner.java
@@ -1,2 +1,43 @@
 public class Owner {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Owner(String firstName, String lastName, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setFirstName(){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(){
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(){
+        this.phoneNumber = phoneNumber;
+    }
+
+//    public static void main(String[] args){
+//        Owner owner = new Owner("Helen","Dai", "63661076");
+//        System.out.println(owner.getFirstName());
+//        System.out.println(owner.getLastName());
+//        System.out.println(owner.getPhoneNumber());
+//
+//    }
 }
